0bac94392cea9e3c7ecf93f0f3bee516099ae45f
..jh <>
2026-02-19 19:52:45 +0400

    s

---
+++ MyClass/script17.js
@@ -0,0 +1,8 @@
+const paragraphs = document.querySelectorAll("p");
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.border = "black";
+  paragraphs[i].style.background = "blue";
+  paragraphs[i].style.color = "red";
+  paragraphs[i].style.marginTop = "100px";
+  paragraphs[i].innerHTML = "zdddd";
+}
