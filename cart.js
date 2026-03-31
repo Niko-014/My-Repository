a969d6c28fbdff020f649f71d07ee8f6b534dbfc
Niko-014 <zara.nikoloz@gmail.com>
2026-03-31 19:59:21 +0400

    s

---
+++ cart.js
@@ -0,0 +1,6 @@
+<article class="cart-item">
+  <div class="item-image"></div>
+  <h2 class="item-name">{{cartItem.name}}</h2>
+  <p class="item-meta">Qty: {{cartItem.quantity}}</p>
+  <p class="item-price">${{cartItem.price}}</p>
+</article>
